3f86a23ea88d4fc15ae14cdd52fdd4c058c1606d
Rando Wiltschek <wiltschek@gmx.net>
2024-04-27 13:08:12 +0200

    - Fix: Pawns and guests now get mood buff from sleep accelerator

--- Source/Source/Patches/Toils_LayDown_Patch.cs
+++ Source/Source/Patches/Toils_LayDown_Patch.cs
@@ -4,81 +4,103 @@ using RimWorld;
 using Verse;
 using BedUtility = Hospitality.Utilities.BedUtility;
 
-namespace Hospitality.Patches
-{
-    public class Toils_LayDown_Patch
-    {
-        /// <summary>
-        /// So guests can think about their bedroom
-        /// </summary>
-        [HarmonyPatch(typeof(Toils_LayDown), nameof(Toils_LayDown.ApplyBedThoughts))]
-        public class ApplyBedThoughts
-        {
-            [HarmonyPrefix]
-            public static bool Replacement(Pawn actor)
-            {
-                if (actor.needs.mood == null) return false;
+namespace Hospitality.Patches;
 
-                Building_Bed building_Bed = actor.CurrentBed();
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInBedroom);
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInBarracks);
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOutside);
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOnGround);
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInCold);
-                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInHeat);
+public class Toils_LayDown_Patch
+{
+    /// <summary>
+    ///     So guests can think about their bedroom
+    /// </summary>
+    [HarmonyPatch(typeof(Toils_LayDown), nameof(Toils_LayDown.ApplyBedThoughts))]
+    public class ApplyBedThoughts
+    {
+        [HarmonyPrefix]
+        public static bool Replacement(Pawn actor)
+        {
+            if (actor.needs.mood == null) return false;
+
+            var bed = actor.CurrentBed(); // ADDED to make sure we also get guest's beds
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInBedroom);
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInBarracks);
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOutside);
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOnGround);
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInCold);
+            actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInHeat);
+
+            var ambientTemperature = actor.AmbientTemperature;
+            var comfyTemperatureMin = actor.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMin);
+            var comfyTemperatureMax = actor.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMax);
+            if (ModsConfig.BiotechActive && actor.genes != null)
+            {
+                foreach (var item in actor.genes.GenesListForReading)
+                {
+                    if (!item.Active) continue;
+                    comfyTemperatureMin += item.def.statOffsets.GetStatOffsetFromList(StatDefOf.ComfyTemperatureMin);
+                    comfyTemperatureMax += item.def.statOffsets.GetStatOffsetFromList(StatDefOf.ComfyTemperatureMax);
+                }
+            }
+
+            if (ambientTemperature < comfyTemperatureMin)
+            {
+                actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptInCold);
+            }
+
+            if (ambientTemperature > comfyTemperatureMax)
+            {
+                actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptInHeat);
+            }
+
+            if (!actor.IsWildMan())
+            {
                 if (actor.GetRoom().PsychologicallyOutdoors)
                 {
                     actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptOutside);
                 }
-                if (building_Bed == null || building_Bed.CostListAdjusted().Count == 0)
+
+                if (bed == null || bed.CostListAdjusted().Count == 0)
                 {
                     actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptOnGround);
                 }
-                if (actor.AmbientTemperature < actor.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMin))
-                {
-                    actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptInCold);
-                }
-                if (actor.AmbientTemperature > actor.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMax))
-                {
-                    actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SleptInHeat);
-                }
+            }
 
-                if (building_Bed == null || !AddedBedIsOwned(actor, building_Bed) || building_Bed.ForPrisoners || actor.story.traits.HasTrait(TraitDefOf.Ascetic))
-                {
-                    return false;
-                }
+            if (bed != null && AddedBedIsOwned(actor, bed) && !bed.ForPrisoners && !actor.story.traits.HasTrait(TraitDefOf.Ascetic))
+            {
                 ThoughtDef thoughtDef = null;
                 // ADDED:
-                if (building_Bed.GetRoom().Role == InternalDefOf.GuestRoom)
+                if (bed.GetRoom().Role == InternalDefOf.GuestRoom)
                 {
-                    thoughtDef = building_Bed.GetRoom().OnlyOneBed() ? ThoughtDefOf.SleptInBedroom : ThoughtDefOf.SleptInBarracks;
+                    thoughtDef = bed.GetRoom().OnlyOneBed() ? ThoughtDefOf.SleptInBedroom : ThoughtDefOf.SleptInBarracks;
                 } ////
-                else if (building_Bed.GetRoom().Role == RoomRoleDefOf.Bedroom)
+                else if (bed.GetRoom().Role == RoomRoleDefOf.Bedroom)
                 {
                     thoughtDef = ThoughtDefOf.SleptInBedroom;
                 }
-                else if (building_Bed.GetRoom().Role == RoomRoleDefOf.Barracks)
+                else if (bed.GetRoom().Role == RoomRoleDefOf.Barracks)
                 {
                     thoughtDef = ThoughtDefOf.SleptInBarracks;
                 }
+
                 if (thoughtDef != null)
                 {
-                    int scoreStageIndex = RoomStatDefOf.Impressiveness.GetScoreStageIndex(building_Bed.GetRoom().GetStat(RoomStatDefOf.Impressiveness));
+                    var scoreStageIndex = RoomStatDefOf.Impressiveness.GetScoreStageIndex(bed.GetRoom().GetStat(RoomStatDefOf.Impressiveness));
                     if (thoughtDef.stages[scoreStageIndex] != null)
                     {
                         actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtMaker.MakeThought(thoughtDef, scoreStageIndex));
                     }
                 }
-                return false;
             }
 
-            // Added
-            private static bool AddedBedIsOwned(Pawn pawn, Building_Bed buildingBed)
-            {
-                return pawn.IsArrivedGuest(out _) 
-                    ? BedUtility.GetGuestBed(pawn) == buildingBed 
-                    : buildingBed == pawn.ownership.OwnedBed;
-            }
+            actor.Notify_AddBedThoughts();
+
+            return false;
+        }
+
+        // ADDED
+        private static bool AddedBedIsOwned(Pawn pawn, Building_Bed buildingBed)
+        {
+            return pawn.IsArrivedGuest(out _)
+                ? BedUtility.GetGuestBed(pawn) == buildingBed
+                : buildingBed == pawn.ownership.OwnedBed;
         }
     }
 }
